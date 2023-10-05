25b1319685d226181bc49d7a61d515d344bd229a
Biruk <biruk.tadesse.tefera@gmail.com>
2023-10-05 22:01:06 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home({ githubProfileData }) {
       <Proficiency />
       <Education />
       <Experience />
+      <Feedbacks />
       <Projects />
       {/* <Feedbacks /> */}
       {/* {showContactUs ? <Contact /> : null} */}
