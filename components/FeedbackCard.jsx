d6d3b942292807cd0f240742132a4dc23180f82a
Biruk <biruk.tadesse.tefera@gmail.com>
2024-05-20 16:24:53 +0300

    Update FeedbackCard.jsx

--- components/FeedbackCard.jsx
+++ components/FeedbackCard.jsx
@@ -8,6 +8,22 @@ const FeedbackCard = ({ data }) => {
 		<Fade bottom duration={2000} >
 			<Card className="card-lift--hover shadow mt-4">
 				<CardBody>
+				<img
+							src={data.logo}
+							style={{
+								objectFit: "cover",
+								left: 0,
+								right: 0,
+								top: "7rem",
+								marginLeft: "auto",
+								marginRight: "auto",
+								width: "8rem",
+								height: "8rem",
+								borderRadius: "50%",
+							}}
+							className="shadow mb-3"
+							alt={data.logo}
+						/>
 					<div className="d-flex px-3">
 						<div className="pl-4">
 							<h5 className="mb-2">{data.certificationName}</h5>
