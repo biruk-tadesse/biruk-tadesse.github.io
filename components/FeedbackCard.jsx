32c81323c7bf7f82883a2eb8feb1f38a21dd132f
Biruk <biruk.tadesse.tefera@gmail.com>
2024-05-20 15:48:24 +0300

    Update FeedbackCard.jsx

--- components/FeedbackCard.jsx
+++ components/FeedbackCard.jsx
@@ -1,84 +1,4 @@
 import React from "react";
-
-
-import {
-	Card,
-	CardBody,
-	CardTitle,
-	CardSubtitle,
-	CardText,
-	Col,
-} from "reactstrap";
-
-import { Fade } from "react-reveal";
-
-const FeedbackCard = ({ data }) => {
-	return (
-		<Col lg="6">
-			<Fade left duration={2000}>
-				<Card
-					style={{ flex: 1 }}
-					className="shadow-lg--hover mb-3 shadow border-0 text-center rounded"
-				>
-					<CardBody className="">
-						<img
-							src={data.logo}
-							style={{
-								objectFit: "cover",
-								left: 0,
-								right: 0,
-								top: "7rem",
-								marginLeft: "auto",
-								marginRight: "auto",
-								width: "8rem",
-								height: "8rem",
-								borderRadius: "50%",
-							}}
-							className="shadow mb-3"
-							alt={data.logo}
-						/>
-						<CardTitle tag="h4" className="mb-2">
-							{data.certificationName}
-						</CardTitle>
-						<CardSubtitle tag="h5" className="mb-2">
-							{data.issueDate}
-						</CardSubtitle>
-						<CardSubtitle>{data.link}</CardSubtitle>
-						<CardText
-							tag="div"
-							className="mb-2"
-						>
-							{data.desc}
-							<ul>
-								{data.descBullets
-									? data.descBullets.map((desc) => {
-											return <li key={desc}>{desc}</li>;
-									  })
-									: null}
-							</ul>
-						</CardText>
-					</CardBody>
-				</Card>
-			</Fade>
-		</Col>
-	);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
 import { Card, CardBody, Badge } from "reactstrap";
 
 import { Fade } from "react-reveal";
@@ -92,7 +12,7 @@ const FeedbackCard = ({ data }) => {
 						<div className="pl-4">
 							<h5 className="mb-2">{data.certificationName}</h5>
 							<p className="mb-2">{data.issueDate}</p>
-              				<p className="mb-2">{data.link}</p>
+              <p className="mb-2">{data.link}</p>
 						</div>
 					</div>
 				</CardBody>
@@ -102,4 +22,3 @@ const FeedbackCard = ({ data }) => {
 };
 
 export default FeedbackCard;
-*/
