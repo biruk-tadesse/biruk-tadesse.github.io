9b45a321923c5bb530d4be5a2863c2c72fdbff88
Biruk <biruk.tadesse.tefera@gmail.com>
2024-05-20 17:01:10 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -199,7 +199,7 @@ export const feedbacks = [
   {
     certificationName: "Meta Full-Stack Engineer Certificate",
     issueDate: "11/01/2024",
-    link: <a href="https://www.credly.com/badges/98f07028-044a-4b02-8cbc-2b0c6193b0e9/public_url" target="_blank"><strong>View</strong></a>,
+    link: <a href="https://www.credly.com/badges/98f07028-044a-4b02-8cbc-2b0c6193b0e9/public_url" target="_blank"><strong>View Certificate</strong></a>,
     logo: "https://images.credly.com/size/340x340/images/997d4586-e7b2-4174-9c76-5c7304953e2c/image.png",
   },
   {
@@ -224,71 +224,85 @@ export const feedbacks = [
     certificationName: "Google IT Support Certificate",
     issueDate: "25/12/2023",
     link: <a href="https://www.credly.com/badges/8fcf9f8c-04f1-4d6d-98b2-b138fb151272/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/fb97a12f-c0f1-4f37-9b7d-4a830199fe84/GCC_badge_IT_Support_1000x1000.png",
   },
   {
     certificationName: "Google Digital Marketing & E-commerce Certificate",
     issueDate: "17/12/2023",
     link: <a href="https://www.credly.com/badges/233b52cd-6c7c-401d-aaab-f75d00dbc4fe/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/941fa490-a052-46ae-beff-1ac8e55c117f/image.png",
   },
   {
     certificationName: "Google Cybersecurity Certificate",
     issueDate: "17/12/2023",
     link: <a href="https://www.coursera.org/account/accomplishments/specialization/AQH43CSB2RUS" target="_blank"><strong>View</strong></a>,
+    logo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~AQH43CSB2RUS/CERTIFICATE_LANDING_PAGE~AQH43CSB2RUS.jpeg",
   },
   {
     certificationName: "Google Cloud Database Engineer Certificate",
     issueDate: "25/12/2023",
     link: <a href="https://www.coursera.org/account/accomplishments/specialization/HH9SU4HJT794" target="_blank"><strong>View</strong></a>,
+    logo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~HH9SU4HJT794/CERTIFICATE_LANDING_PAGE~HH9SU4HJT794.jpeg",
   },
   {
     certificationName: "Securing API Servers",
     issueDate: "18/01/2024",
     link: <a href="https://www.credly.com/badges/8c0c7fe9-6c13-4097-8e1b-effb65065451/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/71296528-e07b-44af-b5cd-7723599793cf/image.png",
   },
   {
     certificationName: "Network Technician",
     issueDate: "23/01/2024",
     link: <a href="https://www.credly.com/badges/6a8be9bc-5ba9-40a7-bd0a-ebd18a4075f1/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/978f88dc-c247-4093-9d39-6efac3651297/image.png",
   },
   {
     certificationName: "AWS Cloud Solutions Architect Certificate",
     issueDate: "25/12/2023",
     link: <a href="https://www.coursera.org/account/accomplishments/specialization/QKXD5SVGXC7B" target="_blank"><strong>View</strong></a>,
+    logo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~QKXD5SVGXC7B/CERTIFICATE_LANDING_PAGE~QKXD5SVGXC7B.jpeg",
   },
   {
     certificationName: "Oracle Cloud Infrastructure 2023 Certified Architect Professional",
     issueDate: "29/09/2023",
     link: <a href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=C298C28BC33BF5AD91405818FFC6F5EBD75E8610002F040D650086BA8715E00E" target="_blank"><strong>View</strong></a>,
+    logo: "https://brm-workforce.oracle.com/pdf/certview/images/OCICAP2023OPN.png",
   },
   {
     certificationName: "Oracle Cloud Infrastructure 2023 Certified Data Science Professional",
     issueDate: "19/09/2023",
     link: <a href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=CABE57EB0CBE8F34A57229DCAA6B7652225DF6C200776FF432BD862CB84A24C0" target="_blank"><strong>View</strong></a>,
+    logo: "https://brm-workforce.oracle.com/pdf/certview/images/OCIDSP2023OPN.png",
   },
   {
     certificationName: "Oracle Cloud Infrastructure 2023 Certified DevOps Professional",
     issueDate: "18/09/2023",
     link: <a href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=C6AE2F0EAE290EC8B92EEB75F4BFC51F5FA95CD86D427B0FF00316E5E57CDE13" target="_blank"><strong>View</strong></a>,
+    logo: "https://brm-workforce.oracle.com/pdf/certview/images/OCIDOP2023OPN.png",
   },
   {
     certificationName: "Oracle Cloud Infrastructure 2023 Certified Developer Professional",
     issueDate: "17/09/2023",
     link: <a href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=25B4C4B4E9D8C72AF9D3FF320105A456296A4D84D987AF7B2BB50A46199FA10E" target="_blank"><strong>View</strong></a>,
+    logo: "https://brm-workforce.oracle.com/pdf/certview/images/OCID2023CP.png",
   },
   {
     certificationName: "ALX Software Engineering Programme ┊ Back End Development",
     issueDate: "29/11/2023",
     link: <a href="https://intranet.alxswe.com/certificates/fRr63TxcFC" target="_blank"><strong>View</strong></a>,
+    logo: "https://intranet.alxswe.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBa1FaIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3e0a6838a1b0688633b3901180edc6dd110d45a5/17-short-specializations-certificate-biruk-tadesse-tefera.png",
   },
   {
     certificationName: "Artificial Intelligence Analyst Mastery Award",
     issueDate: "14/12/2019",
     link: <a href="https://www.credly.com/badges/0e13a1b3-ff97-49d0-81f1-a8e249a14645/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/2fb2af02-750e-4f2c-8a0b-98f3a6d0144b/AI_Analyst_-_Mastery_2019.png",
   },
   {
     certificationName: "Big Data Engineer on Cloud Mastery Award",
     issueDate: "12/10/2019",
     link: <a href="https://www.credly.com/badges/58be4851-64ef-49f7-8c28-22157fec52eb/public_url" target="_blank"><strong>View</strong></a>,
+    logo: "https://images.credly.com/size/340x340/images/16f55d45-8490-499f-a6ad-3fa900d3f0a8/Big_Data_Eng_-_Mastery_2018_-_2020.png",
   },
 
 ];
