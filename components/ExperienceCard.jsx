69b7eac67ee22ad109c36ec66e46fffa0969afc9
Biruk <biruk.tadesse.tefera@gmail.com>
2024-05-23 13:30:52 +0300

    Update ExperienceCard.jsx

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -1,35 +1,53 @@
 import React from "react";
-import { Card, CardBody, Badge } from "reactstrap";
+
+import {
+	Card,
+	Badge,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	CardText,
+	Col,
+} from "reactstrap";
 
 import { Fade } from "react-reveal";
 
 const ExperienceCard = ({ data }) => {
 	return (
-		<Fade left duration={2000} >
-			<Card className="card-lift--hover shadow mt-4">
-				<CardBody>
-					<div className="text-center mb-3">
+		<Col lg="6">
+			<Fade left duration={2000}>
+				<Card
+					style={{ flex: 1 }}
+					className="shadow-lg--hover mb-3 shadow border-0 text-center rounded"
+				>
+					<CardBody className="">
 						<img
-							src={data.logo}
+							src={data.companylogo}
 							style={{
 								objectFit: "cover",
 								width: "8rem",
 								height: "8rem",
 								borderRadius: "50%",
 							}}
-							className="shadow"
+							className="shadow mb-3"
 							alt={data.companylogo}
-							/>
-							</div>
-							<div className="text-center">
-							<h5 className="mb-2">
-								{data.company}
-							</h5>
-								<h6>{data.role}</h6>
-							<Badge color="info" className="mr-1">
-								{data.date}
-							</Badge>
-							
+						/>
+
+						<CardTitle tag="h4" className="mb-2">
+							{data.company}
+						</CardTitle>
+						<CardSubtitle tag="h5" className="mb-2">
+							{data.role}
+						</CardSubtitle>
+						<CardSubtitle>
+						<Badge color="info" className="mr-1">
+						{data.date}
+						</Badge>
+						</CardSubtitle>
+						<CardText
+							tag="div"
+							className="mb-2"
+						>
 							<p className="mb-2">{data.desc}</p>
 							<ul>
 								{data.descBullets
@@ -38,10 +56,11 @@ const ExperienceCard = ({ data }) => {
 									  })
 									: null}
 							</ul>
-						</div>
-				</CardBody>
-			</Card>
-		</Fade>
+						</CardText>
+					</CardBody>
+				</Card>
+			</Fade>
+		</Col>
 	);
 };
 
